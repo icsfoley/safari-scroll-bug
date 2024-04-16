18476ef530953cdafcbce8a73fc6fc6c71301cd9
Steve <steve.foley@intercom.io>
2024-04-16 16:54:04 +0000

    Execute `ember new safari-scroll-bug --lang en`

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'safari-scroll-bug/app';
+import config from 'safari-scroll-bug/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
